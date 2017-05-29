59799e90d2667245c7c85e1482828a9642f4b9de
xiaolige945@vip.qq.com <xiaolige945@vip.qq.com>
2017-05-30 00:05:28 +0800

    项目初始化。

---
+++ GameCloud.Manager.Demo/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GameCloud.Manager.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //---------------------------------------------------------------------
+        private readonly ILogger logger;
+
+        //---------------------------------------------------------------------
+        public HomeController(ILoggerFactory logger_factory)
+        {
+            logger = logger_factory.CreateLogger("Default");
+        }
+
+        //---------------------------------------------------------------------
+        public IActionResult Index()
+        {
+            logger.LogInformation("HomeController.Index() ~~~~~~~~");
+
+            return View();
+        }
+
+        //---------------------------------------------------------------------
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        //---------------------------------------------------------------------
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        //---------------------------------------------------------------------
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
